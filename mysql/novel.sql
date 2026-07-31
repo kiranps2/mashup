0fa3ec668f48c17200873444e4e4bd1322363778
kiran ps <kiranpsofficial1@gmail.com>
2026-07-31 10:37:02 +0530

    Add SQL script for creating books table and inserting initial data

--- mysql/novel.sql
+++ mysql/novel.sql
@@ -1,3 +1,4 @@
+CREATE TABLE `book`.`books` (`book_id` INT(25) NOT NULL AUTO_INCREMENT , `title` VARCHAR(25) NOT NULL , `author_id` INT(25) NOT NULL , PRIMARY KEY (`book_id`)) ENGINE = InnoDB;
 ALTER TABLE books add FOREIGN KEY(Author_ID) REFERENCES authors(author_id);
 INSERT INTO authors (NAME, EMAIL) VALUES ('J.K. Rowling', 'jkrowling@example.com'), ('George Orwell', 'georgeorwell@example.com'), ('Jane Austen', 'janeausten@example.com'), ('Mark Twain', 'marktwain@example.com'), ('Agatha Christie', 'agathachristie@example.com');
 INSERT INTO books (BOOK_ID, title,author_id) VALUES (1, 'The Great Gatsby',1), (2, 'To Kill a Mockingbird',2), (3, '1984',3), (4, 'Pride and Prejudice',4), (5, 'The Hobbit',5);
